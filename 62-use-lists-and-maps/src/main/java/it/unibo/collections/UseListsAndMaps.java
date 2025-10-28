1c028edc7930887c8bd53d1292f2e587706ea659
Samuele Rocca - samuele.rocca2@studio.unibo.it <samuele.rocca2@studio.unibo.it>
2025-10-28 16:14:43 +0100

    add first half of exercise 62

--- 62-use-lists-and-maps/src/main/java/it/unibo/collections/UseListsAndMaps.java
+++ 62-use-lists-and-maps/src/main/java/it/unibo/collections/UseListsAndMaps.java
@@ -1,5 +1,7 @@
 package it.unibo.collections;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
@@ -21,19 +23,32 @@ public final class UseListsAndMaps {
          * 1) Create a new ArrayList<Integer>, and populate it with the numbers
          * from 1000 (included) to 2000 (excluded).
          */
+        final List<Integer> listOfIntegers = new ArrayList<Integer>();
+        for (int i = 1000; i < 2000; i++) {
+            listOfIntegers.add(i);
+        }
+        // NOTA : Usare final, finchè non vedi che devi modificarla
+        // NOTA : Nella dichiarazione usare l'interfaccia più generica 
         /*
          * 2) Create a new LinkedList<Integer> and, in a single line of code
          * without using any looping construct (for, while), populate it with
          * the same contents of the list of point 1.
          */
+        final List<Integer> linkedListOfIntegers = new LinkedList<>(listOfIntegers);
         /*
          * 3) Using "set" and "get" and "size" methods, swap the first and last
          * element of the first list. You can not use any "magic number".
          * (Suggestion: use a temporary variable)
          */
+        int last = linkedListOfIntegers.get(linkedListOfIntegers.size()-1);
+        linkedListOfIntegers.set(linkedListOfIntegers.size()-1, linkedListOfIntegers.getFirst());
+        linkedListOfIntegers.set(0, last);
         /*
          * 4) Using a single for-each, print the contents of the arraylist.
          */
+        for (int i : listOfIntegers) {
+            System.out.println(i);
+        }
         /*
          * 5) Measure the performance of inserting new elements in the head of
          * the collection: measure the time required to add 100.000 elements as
